4c1bd1887dd013ea73d0f7e5721be2ac42b52492
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2024-03-07 00:37:09 +0600

    api added:

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 const API_KEY = 'dabf4baad60d538c7c2c22d809003ae3';
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${API_key}`
+}
